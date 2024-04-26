9c7e4017bf86841f3baae5f84f234bb65ba68d94
Abel <abehsosa2004@gmail.com>
2024-04-26 09:49:51 +0200

    added font awesome and dark mode

--- views/templates/navbar.php
+++ views/templates/navbar.php
@@ -1,3 +1,16 @@
+<script>
+
+  const changeMode = ()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+};
+
+</script>
+
 <header class="p-3 mb-3 border-bottom">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -17,17 +30,29 @@
           <li><a href="/abilities" class="nav-link px-2 link-secondary">Listar habilidades</a></li>
         </ul>
 
-        <div class="dropdown text-end">
-          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-          </a>
-          <ul class="dropdown-menu text-small">
-            <li><a class="dropdown-item" href="#">Perfil</a></li>
-            <li><a class="dropdown-item" href="#">Mensajes</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
-          </ul>
+        <div class="text-end">
+          <button class="btn btn-outline-secondary" onclick="changeMode()">
+            <i class="fa-solid fa-moon"></i>
+          </button>
+          <?php if (isset($_SESSION['user'])){ ?>
+              <div class="dropdown">
+                <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu text-small">
+                    <li><a class="dropdown-item" href="#">Perfil</a></li>
+                    <li><a class="dropdown-item" href="#">Mensajes</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+                </ul>
+              </div>
+          <?php } else { ?>
+              <a href="" class="btn btn-primary">Iniciar sesión</a>
+              <a href="" class="btn btn-outline-secondary">Unirse</a>
+          <?php } ?>
         </div>
+
+        
       </div>
     </div>
   </header>
