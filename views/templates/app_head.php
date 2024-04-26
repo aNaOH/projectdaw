4673fc504c80e164db66b0d7ee72e1d73c50ddd3
Abel <abehsosa2004@gmail.com>
2024-04-26 09:56:36 +0200

    added custom css

--- views/templates/app_head.php
+++ views/templates/app_head.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TradeSkill</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
     <link href="/assets/plugins/font-awesome/css/fontawesome.css" rel="stylesheet" />
     <link href="/assets/plugins/font-awesome/css/brands.css" rel="stylesheet" />
     <link href="/assets/plugins/font-awesome/css/solid.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="/assets/css/app.css">
 </head>
 <body>
 <?php include('./views/templates/navbar.php');
